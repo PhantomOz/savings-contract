04c0c7ea02732431d0efda34dd84c3adae4c1eba
PhantomOz <faniogor@gmail.com>
2024-02-15 01:33:39 +0100

    This commit adds deploy script

--- test/SaveEther.ts
+++ test/SaveEther.ts
@@ -1,7 +1,6 @@
 import {
   loadFixture,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
